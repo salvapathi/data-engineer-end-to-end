f048aea59afe09ad6fcdbe1f9435542eb33ad2e4
salvapathi <salvapathinaidu@gmail.com>
2024-03-22 14:24:59 +0530

    first commit

---
+++ scripts/credential.py
@@ -0,0 +1,26 @@
+#MY SQL CREDENTIALS
+your_mysql_host="host.docker.internal"
+your_mysql_user="root"
+your_mysql_password="iscs_user"
+your_mysql_database="automobiles"
+
+#POSTRE SQL CREDENTIALS
+dbname_postgre="churn_customer_2"
+user_postgre="postgres"
+password_postgre="password"
+host_postgre="host.docker.internal"
+port_postgre="5432"
+
+#mino s3 bucket
+access_key= "OhozkLq2hsfAW56Hd9Ta"
+secret_key="W0KW2GWJMl3uehkpm0TDox0Dah9igJPIsQkWUrSe"
+endpoint = "http://host.docker.internal:9000"
+
+#mongodb
+server=MongoClient('mongodb://host.docker.internal:27017/')
+data_base_name='automobiles'
+collections_name='super_bikes'
+
+
+
+
